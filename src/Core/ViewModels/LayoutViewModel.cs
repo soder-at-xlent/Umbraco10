94fdf46a0a04c695af794686a70aa3e84cd0b5d2
jonas Söderström <jonas.soderstrom@xlent.se>
2022-09-29 15:49:13 +0200

    Add simple menu

--- src/Core/ViewModels/LayoutViewModel.cs
+++ src/Core/ViewModels/LayoutViewModel.cs
@@ -8,11 +8,22 @@ public class LayoutViewModel : ContentModel
     protected LayoutViewModel(
         IPublishedContent content) : base(content)
     {
-        var titleProperty = content.Value<string>("title");
+        var titleProperty = content.Value<string>("title", fallback: Fallback.ToAncestors);
         Title = !string.IsNullOrWhiteSpace(titleProperty) ? titleProperty : content.Name;
+        Subtitle = content.Value<string>("subtitle", fallback: Fallback.ToAncestors);
+        MenuItems = content.Root().Children
+            .Select(child => new LayoutMenuItem { Name = child.Name, Url = child.Url() }).ToList();
     }
 
     public string Title { get; set; }
+    public string Subtitle { get; set; }
+    public IReadOnlyCollection<LayoutMenuItem> MenuItems { get; set; }
+}
+
+public class LayoutMenuItem
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
 }
 
 public class LayoutViewModel<T> : LayoutViewModel where T : class
